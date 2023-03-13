1991e82c465d7e40abbafba685afbf99d55fc9fb
itsabishiek <itsabishiek@gmail.com>
2023-03-13 11:59:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react'
+import logo from "./assets/logo.png"
+import './App.css'
+import { Card, CardContent, CircularProgress } from '@mui/material';
+import { FitnessCenter } from '@mui/icons-material';
+
+const App = () => {
+  const [fitData, setFitData] = useState([]);
+  const [muscle, setMuscle] = useState("biceps");
+  const [loading, setLoading] = useState(false);
+
+  const fetchApi = async () => {
+    const res = await fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}`, {
+      method: "GET",
+      headers: { 
+        'X-Api-Key': "Bo95NtASMIVsW2ixseumQA==W2bxTx1aCo7Ejk2C"
+      },
+    });
+    
+    return res.json();
+  }
+
+  const fetchData = async () => {
+    setLoading(true)
+
+    const data = await fetchApi();
+
+    setFitData(data)
+    setLoading(false);
+
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [muscle])
+
+  console.log(fitData);
+
+  
+
+  return (
+    <div className="app">
+      <div className="heroBanner">
+          <nav>
+            <img className='logo' src={logo} alt="" />
+            <h1>Fitness</h1>
+          </nav>
+      <div className='homeContents'>
+        <h1>Your home fitness hub!</h1>
+        <p>Being a fitness geek is a good thing, as it helps to keep your body healthy and strong. Nowadays, many individuals are becoming more conscious about their health and fitness.</p>
+      </div>
+      </div>
+
+      <div className="actionBtn">
+        <span onClick={() => setMuscle("biceps")}>
+        biceps
+        </span>
+        <span onClick={() => setMuscle("hamstrings")}>hamstrings</span>
+        <span onClick={() => setMuscle("chest")}>chest</span>
+        <span onClick={() => setMuscle("triceps")}>triceps</span>
+      </div>
+
+      {loading && (
+         <CircularProgress sx={{ marginLeft: "15px" }} />
+      )}
+
+      <div className='fitCards'>
+          {fitData.map((f) => (
+        <Card sx={{ width: "100%", marginBottom: 3, bgcolor: "#ffffffc4" }}>
+            <CardContent key={f.name}>
+              <h4>{f.name}</h4>
+              <span>Instructions: </span>
+              <p>{f.instructions}</p>
+            </CardContent>
+        </Card>
+          ))}
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "../App.css"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h3>Fitness</h3>
+        </nav>
+    )
+
+}
+
+export default Navbar;
